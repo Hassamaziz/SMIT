b584c7e280f4df91cccaea3a02b9938c3542d7b3
Hassam Aziz <hassamaziz001@gmail.com>
2023-02-13 15:04:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// Program Number 1
+let name="Umair Azmat";
+admin = name;
+alert(admin);
+
+// Program Number 2
+let plaNet="Earth";
+console.log(plaNet)
+
+// Program Number 3
+let currentVisitor = prompt('Enter Your Name Please:');
+alert('Welcome '+ currentVisitor)
+
+//Program Number 4
+let favDrink  =  String('fanta');
+console.log(favDrink , typeof favDrink)
+
+// Program Number 5
+let favNum = Number(6);
+console.log(favNum , typeof favNum)
+
+// Program Number 6
+ 
+
+// Program Number 7
+    const person={
+        naMe :'umair',
+        age : 20,
+    }  
+    console.log(person);
+
+
+// program number 8
+let a,b,sum;
+a=10;
+b=5;
+sum = a+b;
+console.log('sum of A and B is' ,sum ); 
+
+// Program Number 9
+let no1,no2,reSult;
+no1=20;
+no2=5;
+reSult=20/5;
+console.log('Result of Program 9 =', reSult);
+ 
+// Program Number 10
+let first,second,diVide;
+first=10; second=30;
+diVide=(first+second)/5;
+console.log('Result of program 10= ', diVide);
+
+// Program Number 11
+let C , F
+F=50;
+console.log('Tempurature in Celsius:' , (F-32)*5/9,'°' );
+
+// Program Number 12
+const num1 = Number (prompt('Enter First Number'));
+const num2 = Number (prompt('Enter second Number'));
+
+alert(num1+num2 );
+
+//Program Number 13
+const Num1 = Number (prompt('Enter First Number'));
+const Num2 = Number (prompt('Enter second Number'));
+console.log('Difference is ', Num1-Num2);
+
+// Program Number 14
+const firstNum = Number (prompt('Enter First Number'));
+const secondNum = Number (prompt('Enter second Number'));
+alert(firstNum*secondNum);
+
+// Program Number 15
+const No1 = Number(prompt('Enter first number '));
+const No2 = Number(prompt('Enter second number '));
+console.log('Quotient of Given Number:', No1/No2);
+
+// Program Number 16
+const n1 = Number(prompt('Enter first number'));
+const n2 = Number(prompt('Enter second number'));
+["The Remainder Of given Numbers is " , n1%n2 ].forEach(alert);
+
+
+
+
+
