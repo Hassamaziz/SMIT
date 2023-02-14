78c9e59bcf0c7405f9c8ae9ac2bd67b860d09919
Hassam Aziz <hassamaziz001@gmail.com>
2023-02-14 20:10:24 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// // // Program to find maximum value taken from user using conditional operator
+// let num1 = Number(prompt("Enter First Number"));
+// let num2 = Number(prompt("Enter Second Number"));
+
+// if (num1 > num2) {
+//   console.log("The Maximum number is ", num1);
+// } else {
+//   console.log("The Maximum Number is ", num2);
+// }
+
+// // Program to find maximum value from three value
+// let a=500;
+// let b=100;
+// let c=600;
+// if(a>b)
+// {
+//     console.log(a,'is maximum');
+// }
+// else if (b>c) {
+//     console.log(b ,'is maximum');
+    
+// } else {
+//     console.log(c , 'is maximum');
+    
+// }
+
+// program to check if number is int 
+
+let nuM1 = parseInt(prompt('Enter Number to Check'));
+if(Number.isInteger(nuM1)){
+    console.log('number is INT');
+}
+else{
+    console.log('number is not INT');
+}
+
