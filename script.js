693146d01f4091be9ecfef634175f271138349b9
Hassamaziz <118983911+Hassamaziz@users.noreply.github.com>
2023-02-15 20:38:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,36 @@
-// Program Number 1
-let name="Umair Azmat";
-admin = name;
-alert(admin);
+// // // Program to find maximum value taken from user using conditional operator
+// let num1 = Number(prompt("Enter First Number"));
+// let num2 = Number(prompt("Enter Second Number"));
 
-// Program Number 2
-let plaNet="Earth";
-console.log(plaNet)
+// if (num1 > num2) {
+//   console.log("The Maximum number is ", num1);
+// } else {
+//   console.log("The Maximum Number is ", num2);
+// }
 
-// Program Number 3
-let currentVisitor = "visitor name";
+// // Program to find maximum value from three value
+// let a=500;
+// let b=100;
+// let c=600;
+// if(a>b)
+// {
+//     console.log(a,'is maximum');
+// }
+// else if (b>c) {
+//     console.log(b ,'is maximum');
+    
+// } else {
+//     console.log(c , 'is maximum');
+    
+// }
 
-//Program Number 4
-let favDrink  =  'fanta';
+// program to check if number is int 
 
-// Program Number 5
-let favNum = "6";
-
-// Program Number 6
- 
-
-// Program Number 7
-    const person={
-        naMe :'umair',
-        age : 20,
-    }  
-    console.log(person);
-
-
-// program number 8
-let a,b,sum;
-a=10;
-b=5;
-sum = a+b;
-console.log('sum of A and B is' ,sum ); 
-
-// Program Number 9
-let no1,no2,reSult;
-no1=20;
-no2=5;
-reSult=20/5;
-console.log('Result of Program 9 =', reSult);
- 
-// Program Number 10
-let first,second,diVide;
-first=10; second=30;
-diVide=(first+second)/5;
-console.log('Result of program 10= ', diVide);
-
-// Program Number 11
-let C , F
-F=50;
-console.log('Tempurature in Celsius:' , (F-32)*5/9,'°' );
-
-// Program Number 12
-const num1 = Number (prompt('Enter First Number'));
-const num2 = Number (prompt('Enter second Number'));
-
-alert(num1+num2 );
-
-//Program Number 13
-const Num1 = Number (prompt('Enter First Number'));
-const Num2 = Number (prompt('Enter second Number'));
-console.log('Difference is ', Num1-Num2);
-
-// Program Number 14
-const firstNum = Number (prompt('Enter First Number'));
-const secondNum = Number (prompt('Enter second Number'));
-alert(firstNum*secondNum);
-
-// Program Number 15
-const No1 = Number(prompt('Enter first number '));
-const No2 = Number(prompt('Enter second number '));
-console.log('Quotient of Given Number:', No1/No2);
-
-// Program Number 16
-const n1 = Number(prompt('Enter first number'));
-const n2 = Number(prompt('Enter second number'));
-["The Remainder Of given Numbers is " , n1%n2 ].forEach(alert);
-
-
-
-
+let nuM1 = parseInt(prompt('Enter Number to Check'));
+if(Number.isInteger(nuM1)){
+    console.log('number is INT');
+}
+else{
+    console.log('number is not INT');
+}
 
