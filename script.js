1a592ca4bd54e8c2cf243435cac75ac436dbb3ff
Hassam Aziz <hassamaziz001@gmail.com>
2023-02-26 22:58:50 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,130 @@
+// // program 2 
+
+//  console.log(5>4); //true
+//  console.log('apple'>'pineapple'); //false
+// console.log('2'>'12'); //true
+// console.log(undefined == null); //true
+// console.log(undefined === null); //false
+// console.log(null == "\n0\n" ); //false
+// console.log(null === +"\n0\n"); //false
+
+// // program number 3
+// let a = 1;
+// let b = 1;
+// let c = ++a;
+// let d = b++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// // program 4
+// let A = 2;
+// let x = 1+(A *= 2);
+// console.log(A);
+// console.log(x);
+
+// // program 5
+// // in the prompt line there should be a constraints added of Number or ParseInt
+// // let num1 = Number(prompt('first number' ,1));
+// // let num2 = Number(prompt('second number', 2));
+// // alert(num1+num2);
+
+// // program 6
+
+// let aA=1;
+// bB=2;
+// (aA + bB < 4) ? console.log('below'): console.log('over');
+
+// program 7
+
+// let logIn = prompt('enter login');
+
+// (logIn == 'Employee')?
+// console.log('Hello'):
+// console.log('');
+
+// (logIn == 'Director')?
+// console.log('Greetings'):
+// console.log('');
+
+// (logIn == '')?
+// console.log('No login'):
+// console.log('');
+
+// // program 8
+// alert(null || 2 || undefined); // 2 = output
+
+// // program 9
+// alert(alert(1)||2||alert(3)); // 1 and 2 output
+
+// // program 10
+// alert(1 && null && 2); //null output
+
+// program 11
+// alert(alert(1) && alert(2)); // 1 and undefined
+
+// program 12
+// alert(null || 2 && 3 || 4); //3 output
+
+// program 13 if condition variant one
+// let age = 13;
+// if(age>=14 && age<= 94){
+//     console.log("age is between 14 and 94");
+// }
+// else{
+//     console.log('age is not beteen 14 and 94');
+// }
+// program 13 variant two
+
+// let aGe = 90;
+// if (!(aGe >= 14 && aGe <= 94)) {
+//   console.log('age is not between 14 to 94');
+// }
+
+
+// else{
+//     console.log('age is between 14 to 94');
+// }
+
+// program 14
+
+// if( -1 || 0 ){
+//     alert('first');   //will print
+// }
+
+// if(-1&&0){
+//     alert('second'); //will not print
+// }
+// if(null||-1&&1){
+//     alert('third'); //will print
+// }
+
+// program 15
+
+// let browSer = prompt('enter your browser');
+// if(browSer=='Edge'){
+//     alert('you have got the Edge!');
+// }
+// else if(browSer=='Chrome'||browSer=='Firefox'|| browSer=='Safari'||browSer=='Opera'){
+//     alert('Okay we support these browsers too');
+// }
+// else{
+//     alert('we hope that this page looks ok!');
+// }
+
+// program 16
+// let a = +prompt('a?', '');
+
+// switch(a) {
+//   case 0:
+//     alert(0);
+//     break;
+//   case 1:
+//     alert(1);
+//     break;
+//   case 2:
+//   case 3:
+//     alert('2,3');
+//     break;
+// }
